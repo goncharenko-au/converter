242786067901396aa1011a551922b85e3544484c
Alina <goncharenko0206@gmail.com>
2021-04-17 19:53:32 +0300

    добавила axios, вынесла state в родителя

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 import Input from "./components/Input";
-import Header from "./components/Header"
+import Header from "./components/Header";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [curr, setCurr] = useState(0);
+  const [select, setSel] = useState("EUR")
+
+  async function getRate(num) {
+    axios.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+      .then(response => response.data)
+      .then(data => data.find(item => item.cc === select))
+      .then(object => object.rate)
+      .then(rate => setCurr(rate));
+  }
+
+  useEffect(() => {
+    getRate(value);
+  }, [select])
+
   return (
-    <>
-      <Header />
-      <Input />
-    </>
+    <Fragment>
+      <div className="wrapper">
+        <Header />
+        <Input value={value} setValue={setValue} curr={curr} />
+        <select value={select} onChange={(e) => setSel(e.target.value)}>
+          <option>USD</option>
+          <option>EUR</option>
+          <option>AUD</option>
+          <option>CAD</option>
+          <option>CNY</option>
+        </select>
+        <input value={(curr * +value).toFixed(1)} />
+      </div>
+    </Fragment>
   )
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,30 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-export default function Input() {
-    const [value, setValue] = useState("");
-    const [curr, setCurr] = useState(0);
-    // let val = null;
+export default function Input(props) {
+    // const [curr, setCurr] = useState(0);
 
-    async function r(v2) {
-        let response = await fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
-        let format = await response.json();
-        let search = await format.find(item => item.cc === "USD");
-        let val = await search.rate;
-        return setCurr(val);
 
-    }
-    useEffect(() => {
-        r(value);
-    }, [])
+    // async function getRate(v2) {
+    //     let response = await fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+    //     let format = await response.json();
+    //     let search = await format.find(item => item.cc === "USD");
+    //     let val = await search.rate;
+    //     return setCurr(val);
+    // }
+
+    // useEffect(() => {
+    //     getRate(props.value);
+    // }, [])
 
 
     return (
         <>
             <input placeholder="Введите"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
+                value={props.value}
+                onChange={(e) => props.setValue(e.target.value)}
             />
-            <span >Результат:{(curr * +value).toFixed(2)}</span>
+
         </>
     )
 }
